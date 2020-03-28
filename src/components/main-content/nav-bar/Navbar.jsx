37701425f8d500f8e3730a0bd38529633409e7cd
re7 <alhayek_ali@outlook.com>
2020-03-28 13:32:16 +0100

    nav bar

--- src/components/main-content/nav-bar/Navbar.jsx
+++ src/components/main-content/nav-bar/Navbar.jsx
@@ -1,17 +1,46 @@
-import React, { Component } from "react";
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import Switch from "@material-ui/core/Switch";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormGroup from "@material-ui/core/FormGroup";
+import MenuItem from "@material-ui/core/MenuItem";
+import Menu from "@material-ui/core/Menu";
 import Search from "./search/Search";
-import Signin from "./sign-in/Signin";
 import Navmenu from "./nav-menu/Navmenu";
+import Signin from "./sign-in/Signin";
 import "./nav-bar.css";
 
-const Navbar = props => {
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1
+  },
+
+  menuButton: {
+    marginRight: theme.spacing(2)
+  },
+  title: {
+    flexGrow: 1
+  }
+}));
+
+export default function Navbar() {
+  const classes = useStyles();
+
   return (
-    <div className="nav-bar-container">
-      <Search />
-      <Navmenu />
-      <Signin />
+    <div className={classes.root} className="full-width">
+      <AppBar position="static">
+        <Toolbar>
+          <Search />
+
+          <Navmenu />
+        </Toolbar>
+      </AppBar>
     </div>
   );
-};
-
-export default Navbar;
+}
--- src/components/main-content/nav-bar/nav-menu/Navmenu.jsx
+++ src/components/main-content/nav-bar/nav-menu/Navmenu.jsx
@@ -1,20 +1,72 @@
 import React from "react";
-import Language from "../switch-language/Language";
+import Signin from "../sign-in/Signin";
 import "./nav-menu.css";
-const Navmenu = props => {
+
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import FolderIcon from "@material-ui/icons/Folder";
+import RestoreIcon from "@material-ui/icons/Restore";
+import FavoriteIcon from "@material-ui/icons/Favorite";
+import LocationOnIcon from "@material-ui/icons/LocationOn";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    height: 80,
+    background: "transparent !important",
+    display: "flex"
+  }
+});
+
+export default function Navmenu() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
-    <div className="nav-menu-container">
-      <ul className="nav-menu">
-        <li className="nav-item">Our Company</li>
-        <li className="nav-item">News</li>
-        <li className="nav-item">Our vision</li>
-        <li className="nav-item">About us</li>
-        <li className="nav-item">Contact us</li>
-      </ul>
-
-      <Language />
-    </div>
+    <BottomNavigation
+      value={value}
+      onChange={handleChange}
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        label="Our Company"
+        value="Our Company"
+        icon={<RestoreIcon />}
+      />
+      <BottomNavigationAction
+        label="News"
+        value="News"
+        icon={<FavoriteIcon />}
+      />
+      <BottomNavigationAction
+        label="About us"
+        value="About us"
+        icon={<LocationOnIcon />}
+      />
+      <BottomNavigationAction
+        label="Contact us"
+        value="Contact us"
+        icon={<FolderIcon />}
+      />
+      <Signin />
+    </BottomNavigation>
   );
-};
+}
+{
+  /* <div className="nav-menu-container">
+  <ul className="nav-menu">
+    <li className="nav-item">Our Company</li>
+    <li className="nav-item">News</li>
+    <li className="nav-item">Our vision</li>
+    <li className="nav-item">About us</li>
+    <li className="nav-item">Contact us</li>
+  </ul>
 
-export default Navmenu;
+  <Language />
+</div> */
+}
