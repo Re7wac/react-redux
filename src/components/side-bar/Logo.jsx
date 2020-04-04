e439b4b4c8ebdc1c86d3dc3283dc216b98b238c2
aalkhalil <aalkhalil@vjoon.com>
2020-04-04 14:54:56 +0200

    reafctore theme button

--- src/components/side-bar/Logo.jsx
+++ src/components/side-bar/Logo.jsx
@@ -8,13 +8,14 @@ const useStyles = makeStyles(theme => ({
     boxShadow:
       "-6px 2px 4px -1px rgba(0,0,0,0.2), -6px 4px 5px 0px rgba(0,0,0,0.14), -6px 1px 10px 0px rgba(0,0,0,0.12)",
     position: "relative",
-    zIndex: 2
+    zIndex: 2,
+    borderRadius: 0
   }
 }));
 const Logo = props => {
   const classes = useStyles("");
   return (
-    <Paper className={classes.root} elevation="0" square="false">
+    <Paper className={classes.root}>
       <Link to="#" className="nav-link">
         <i className="fas fa-angle-double-right fa-secondary"></i>
         <span className="link-text">RE7</span>
